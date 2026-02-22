4a76cbddb3671a384241247724cd9e24d9059a3e
Milan Trnka <milan.trnka@maxferit.com>
2026-02-22 23:08:41 +0100

    chore: uklid fix scriptu

--- fix-dup.py
+++
@@ -1,6 +0,0 @@
-﻿import os
-p = os.path.join('workers', 'admin-api', 'src', 'index.ts')
-c = open(p, encoding='utf-8').read()
-c = c.replace('const logRes = const logRes =', 'const logRes =')
-open(p, 'w', encoding='utf-8').write(c)
-print('OK - duplikat opraven')
